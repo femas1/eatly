7146e0c7335bffbc3c7e27e9ed7a9dcb967dd4eb
femas1 <federico.massara2@gmail.com>
2022-11-29 11:42:43 +0100

    add js and test api call (random recipe)

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// API TEST CALL 
+
+const getRandRecipe = async function () {
+    const res = await fetch('https://api.spoonacular.com/recipes/random?apiKey=b69e38af682b4e7fa423de0c87c3e848');
+    const data = await res.json();
+
+    const title = data.recipes[0].title;
+    const image = data.recipes[0].image;
+    const readyIn = data.recipes[0].readyInMinutes;
+    const servings = data.recipes[0].servings
+    const instructions = data.recipes[0].instructions;
+    const extIngredients = data.recipes[0].extendedIngredients;
+};
+
+getRandRecipe();
