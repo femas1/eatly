e7d45751d59a39cea65a20789d18bb756b204718
femas1 <federico.massara2@gmail.com>
2022-11-30 11:55:04 +0100

    add async function for searching recipe by ingredient

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const ingredientName = document.querySelector('.container__content__mainContent_
 const previewPicture = document.getElementById('preview-picture');
 const recipeTitle = document.querySelector('.container__content__sidebar__recipe__title');
 const recipeDirections = document.querySelector('.container__content__mainContent__directions__text');
-const cookingTime = document.querySelector('.container__content__mainContent__dishInfo__time span')
+const cookingTime = document.querySelector('.container__content__mainContent__dishInfo__time span');
+const recipeServings = document.querySelector('.container__content__mainContent__dishInfo__servings span');
 
 
 // API TEST CALL 
@@ -33,12 +34,23 @@ const getRandRecipe = async function () {
     previewPicture.src = image;
     recipeDirections.innerText = instructions;
     cookingTime.innerText = readyIn;
+    recipeServings.innerText = servings;
 };
 
-getRandRecipe();
+// getRandRecipe();
 
 // SCENARIO: User starts the app 
 
 // 1. Page loads
 // 2. Show all recipes in the sidebar
+    // 2.1. search recipe based on random ingredient (random ingredient from array)
+
 // 3. Open / show the first recipe of the list 
+
+const searchRecipes = async function () {
+    const res = await fetch('https://api.spoonacular.com/recipes/findByIngredients?apiKey=b69e38af682b4e7fa423de0c87c3e848&ingredients=cheddar');
+    const data = await res.json();
+    console.log(data);
+}
+
+// searchRecipes();
